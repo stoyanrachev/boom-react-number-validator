937cb626ea3e06d29c1751cc4c6abfb610d2dd8c
DESKTOP-IN4R64E\strac <strachev@gmail.com>
2023-01-25 16:03:05 +0200

    coding this logic

--- src/App.js
+++ src/App.js
@@ -1,18 +1,46 @@
 import "./App.css";
+import { useState, useMemo } from "react";
+
+
+function isNumber(str) {
+  const re = /^[0-9\b]+$/;
+  if (!re.test(str)) {
+    return false;
+  }
+
+  return !isNaN(str);
+}
 
 function App() {
+  const [input, setNum] = useState('');
+  const [icon, setIcon] = useState('fa-times');
+
+
+  const calculation = useMemo(() => isNumber(input), [input]);
+
+  const handleChange = event => {
+    setNum(event.target.value);
+
+    if (calculation) {
+      setIcon('fa-check');
+    } else {
+      setIcon('fa-times');
+    }
+  };
+
   return (
     <div className="App">
-     <div className="control has-icons-right">
+      <div className="control has-icons-right">
         <input
           className="input is-large"
           type="text"
           placeholder="Enter number..."
-
+          onChange={handleChange}
+          value={input}
         />
         <span className="icon is-small is-right">
-          <i className="fas fa-times" />
-        </span> 
+          <i className={"fas " + icon} />
+        </span>
       </div>
     </div>
   );
